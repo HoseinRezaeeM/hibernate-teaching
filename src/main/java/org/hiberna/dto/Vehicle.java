a5c9850b9ed05f685042800e08784ae287fff98d
HoseinRezaei <rezaeen77@gmail.com>
2023-11-07 16:56:21 +0330

    many to one

--- src/main/java/org/hiberna/dto/Vehicle.java
+++ src/main/java/org/hiberna/dto/Vehicle.java
@@ -1,14 +1,24 @@
 package org.hiberna.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Vehicle {
 @Id @GeneratedValue
     private int VehicleId;
     private String VehicleName;
+    @ManyToOne
+    @JoinTable(name = "USER_VEHICLE",joinColumns = @JoinColumn(name = "VEHICLE_ID")
+            ,inverseJoinColumns = @JoinColumn(name = "USER_ID"))
+    private UserDetail userDetail;
+
+    public UserDetail getUserDetail() {
+        return userDetail;
+    }
+
+    public void setUserDetail(UserDetail userDetail) {
+        this.userDetail = userDetail;
+    }
 
     public int getVehicleId() {
         return VehicleId;
