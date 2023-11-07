222a0e09e6e693c344241807f6edb832723ecc3f
HoseinRezaei <rezaeen77@gmail.com>
2023-11-07 12:35:29 +0330

    edit Address class

--- src/main/java/org/hiberna/dto/Address.java
+++ src/main/java/org/hiberna/dto/Address.java
@@ -1,12 +1,17 @@
 package org.hiberna.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+
 
 
 @Embeddable
 public class Address {
+  @Column(name = "STREET")
     private String street;
+  @Column(name = "CITY")
     private String city;
+  @Column(name = "STATE")
     private String state;
 
     public String getStreet() {
@@ -33,14 +38,5 @@ public class Address {
         this.state = state;
     }
 
-    public String getPincode() {
-        return pincode;
-    }
-
-    public void setPincode(String pincode) {
-        this.pincode = pincode;
-    }
-
-    private String pincode;
 
 }
